8db14c6479ae66dd560ba7f10acc4756b5972f2c
Chinnadurai <rchunna2003@example.com>
2026-06-19 16:23:52 +0530

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,5 +10,15 @@ export default defineConfig({
     },
     build: {
         outDir: 'build',
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                }
+            }
+        }
     },
 });
